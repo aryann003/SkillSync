7dd4e59f7e817015e5b1aae037610b963c261bcd
araynn003 <aryankatiyar642@gmail.com>
2026-05-25 01:50:51 +0530

    day1

---
+++ users/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+
+@api_view(['GET'])
+def test(request):
+    return Response({
+        "message": "skillSync API working"
+    })
+# Create your views here.
