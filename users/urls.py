51c8839b15de952a7aef37ac2baa7130e4b6b02c
araynn003 <aryankatiyar642@gmail.com>
2026-05-25 02:42:37 +0530

    day2

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,17 @@
 from django.urls import path
-from .views import test
+from .views import test, register,profile
 
+
+from rest_framework_simplejwt.views import(
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 urlpatterns = [
     path('test/',test),
+    path('register/',register),
+    path('profile/',profile),
+
+
+    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
--- users/views.py
+++ users/views.py
@@ -2,10 +2,35 @@ from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
+from rest_framework import status
+from .serializers import RegisterSerializer
+
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import permission_classes
 
 @api_view(['GET'])
 def test(request):
     return Response({
         "message": "skillSync API working"
     })
-# Create your views here.
+
+
+@api_view(['POST'])
+def register(request):
+
+    serializer = RegisterSerializer(data=request.data)
+
+    if serializer.is_valid():
+        serializer.save()
+
+        return Response({
+            "message": "User Registration Successful"
+        }, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def profile(request):
+    return Response({
+        "message": f"welcome {request.user.username}"
+    })
